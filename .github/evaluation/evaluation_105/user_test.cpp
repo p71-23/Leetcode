aa00d7fb96c9320b50324a31af68d08f34ea9048
p71-23 <125045207+p71-23@users.noreply.github.com>
2023-08-28 00:23:18 +0800

    Update user_test.cpp

--- .github/evaluation/evaluation_105/user_test.cpp
+++ .github/evaluation/evaluation_105/user_test.cpp
@@ -2,12 +2,7 @@
 #include<stdlib.h>
 #include<time.h>
 #include "DATA_STRUCTURES.h"
-//二叉树定义
-struct TreeNode{
-  int val;
-  struct TreeNode* left;
-  struct TreeNode* right;
-};
+
 struct TreeNode* buildTree(int* preorder, int preorderSize, int* inorder, int inorderSize){
   //用户自己的代码实现
 
